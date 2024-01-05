c9dac1b1a07a2d51c8540dac1b4601aa9f325dde
maxwalks <78441835+maxwalks@users.noreply.github.com>
2024-01-05 22:15:05 +0100

    fixed main.py, added activate.php

---
+++ activate.php
@@ -0,0 +1,3 @@
+<?php
+exec("python3 main.py");
+?>
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Run command
-command = subprocess.run(['sudo uhubctl -l 1-1 -p 2 -a toggle'], capture_output=True, text=True)
+command = subprocess.run(['uhubctl', '-l', '1-1', '-p', '2', '-a', 'toggle'], capture_output=True, text=True)
 
 # Print output
 print("Output:", command.stdout)
