f696efe24a8b5d4aa232ba7a0e9a2eddb71e4a8e
maxwalks <78441835+maxwalks@users.noreply.github.com>
2024-01-05 22:04:42 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import subprocess
+
+# Run command
+command = subprocess.run(['sudo uhubctl -l 1-1 -p 2 -a toggle'], capture_output=True, text=True)
+
+# Print output
+print("Output:", command.stdout)
+print("Return Code:", command.returncode)
