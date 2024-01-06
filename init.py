b1f67e6688d33ff54acc9ae218c9482c3b8a4e4a
maxwalks <78441835+maxwalks@users.noreply.github.com>
2024-01-06 22:18:07 +0100

    commit

--- init.py
+++ init.py
@@ -1,10 +1,14 @@
 from requests import request
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 import subprocess
 
-app = Flask(__name__)
+activate_this_file = "/var/www/html/rpiLed/venv/bin/activate_this.py"
+with open(activate_this_file) as _file:
+        exec(_file.read(), dict(__file__=activate_this_file))
 
-@app.route('/', methods=['POST', 'GET'])
+app = Flask(__name__, template_folder='template')
+
+@app.route('/')
 def index():
     return render_template('index.html')
 
@@ -14,7 +18,7 @@ def toggled():
       # Run command
       command = subprocess.run(['uhubctl', '-l', '1-1', '-a', 'toggle'], capture_output=False, text=True)
       return render_template('index.html')
-      
+
 
 if __name__ == '__main__':
         app.run(debug=True)
