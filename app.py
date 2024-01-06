c5bef856aaa6e79d6f8b397f4cef414c1926948f
maxwalks <78441835+maxwalks@users.noreply.github.com>
2024-01-06 19:53:50 +0100

    improved code

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
+from requests import request
 from flask import Flask, render_template, jsonify
-import test
+import subprocess
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method == "POST":
-        import subprocess
-        # Run command
-        command = subprocess.run(['uhubctl', '-l', '1-1', '-a', 'toggle'], capture_output=False, text=True)
-
     return render_template('index.html')
+
+@app.route('/toggle-lights/')
+def toggled():
+      import subprocess
+      # Run command
+      command = subprocess.run(['uhubctl', '-l', '1-1', '-a', 'toggle'], capture_output=False, text=True)
+      return render_template('index.html')
+      
+
+if __name__ == '__main__':
+        app.run(debug=True)
