4a341778e9b7c2db47a9ba90cbcefc7dde5e49b4
maxwalks <78441835+maxwalks@users.noreply.github.com>
2024-01-10 21:19:37 +0100

    Cleaned code

--- init.py
+++ init.py
@@ -1,7 +1,6 @@
-from requests import request
 from flask import Flask, render_template
 import subprocess
-import time
+from requests import request
 
 activate_this_file = "/var/www/html/rpiLed/venv/bin/activate_this.py"
 with open(activate_this_file) as _file:
@@ -10,26 +9,25 @@ with open(activate_this_file) as _file:
 app = Flask(__name__, template_folder='template')
 
 @app.route('/')
-def index():
+async def index():
     return render_template('index.html')
 
 @app.route('/toggle-lights/')
-def toggle_lights():
-      import subprocess
-      command = subprocess.run(['uhubctl', '-l', '1-1', '-a', 'toggle'], capture_output=False, text=True)
+async def toggle_lights():
+      subprocess.run(['uhubctl', '-l', '1-1', '-a', 'toggle'], capture_output=False, text=True)
       return render_template('index.html')
 
 @app.route('/lights-on/')
-def lights_on():
+async def lights_on():
        subprocess.run(['uhubctl', '-l', '1-1', '-a', '1'], capture_output=False, text=True)
        return render_template('lights-on.html')
 
 @app.route('/lights-off/')
-def lights_off():
+async def lights_off():
        subprocess.run(['uhubctl', '-l', '1-1', '-a', '0'], capture_output=False, text=True)
        return render_template('lights-off.html')
 
 
 
 if __name__ == '__main__':
-        app.run(debug=True)
+       app.run(debug=True)
