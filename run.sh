0b1b378570589eb7128211a7b9e08922c3a148dd
maxwalks <78441835+maxwalks@users.noreply.github.com>
2024-03-10 10:05:49 +0100

    Config files

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Config files
+sudo cp config/52-usb.rules /etc/udev/rules.d/
+
+# Install tools needed
+sudo apt install git apache2 libapache2-mod-wsgi-py3 pip virtualenv uhubctl
+
+# Config files
+sudo rm /etc/apache2/sites-available/000-default.conf
+sudo cp config/000-default.conf /etc/apache2/sites-available/
+
 # Navigate to the specified directory
 cd /var/www/html
 
@@ -17,3 +27,8 @@ cd rpiLed
 
 # Create a virtual environment
 sudo virtualenv venv
+
+# Reboot System
+echo "Rebooting System..."
+sleep 5
+sudo reboot
